16c062374cffd21714b2022f7b39cb6361e262ee
YofiY <63012939+YofiY@users.noreply.github.com>
2021-01-06 00:12:27 +0100

    Update Main_GA_alg.py

--- Main_GA_alg.py
+++ Main_GA_alg.py
@@ -13,8 +13,8 @@ import math
 #from modules import graphics
 
  
-
-
+	
+	
 def calc_next_head_position(head_position, direction):
 	x, y = direction 
 	next_head_position = (((head_position[0]+(x*const.GRID)) % const.WIDTH), (head_position[1] + (y*const.GRID)) % const.HEIGHT )
@@ -26,10 +26,8 @@ def deltax_deltay(head_position, food_position):
 
 	return delta_x, delta_y
 
-
 def generate_inputs(snake, food_position):    #generates input for absolute directions
 	#MOVES_LIST = [UP, DOWN, LEFT, RIGHT]
-
 	generated_input = []
 	
 	for a, b in const.MOVES_LIST:
@@ -42,9 +40,7 @@ def generate_inputs(snake, food_position):    #generates input for absolute dire
 	
 	generated_input.append(delta_y)
 	generated_input.append(delta_x)
-	
 	#print('GENERATED INPUT = {}'.format(generated_input))
-
 	return generated_input
 
 def generate_inputs2(snake, food_position, relative_directions):    #generates input for relative directions
@@ -67,7 +63,6 @@ def get_output(inputs, neuralNet):
 	output = neuralNet.forward_pass(inputs)
 	#print('OUTPUT LAYER PRE-SOFTMAX = {}'.format(output))
 	processed_output = neuralNet.process_output(output)
-
 	return processed_output
 
 def softmax(x):
@@ -78,12 +73,10 @@ def train(nb_generations):
 
 	nb_input_nodes = 6
 	nb_layer1_nodes = 10
-	nb_layer2_nodes = 10
-	
+	nb_layer2_nodes = 10	
 	nb_output_nodes = 4
 
 	population_size = 500
-
 	performance = []
 	
 	for current_generation in range(nb_generations):
@@ -132,7 +125,6 @@ def train(nb_generations):
 			population = GA.ChildrenGeneration(population_genome, parent_indexes, score_list, population_size, nb_layer1_nodes, nb_layer2_nodes, nb_output_nodes)
 			population_genome = population.generate_population()
 
-
 	return performance
 
 
@@ -147,9 +139,8 @@ def fitness(population_genome, population_size):
 	foodie = food.Food()
 
 	for i in range(population_size):
+		
 		while (snake.trials-1) == i:
-
-			
 			inputs = generate_inputs(snake, foodie.position)
 			output = get_output(inputs, population_genome[i])
 			
@@ -160,36 +151,12 @@ def fitness(population_genome, population_size):
 			#graphics.updateGraphics(snake, foodie, fpsClock, fps)
 
 		score_list = np.append(score_list, snake.score)
-
+	
 	return score_list
-
 
 def selection(score_list, population_size):
 	top20percent = population_size // 5
-
 	return np.argpartition(score_list, -top20percent)[-top20percent:]
-
-
-
-def crossover(dad_Array, mum_Array, n):
-	new_generation = np.zeros((population_size, 3))
-	
-	#keep a copy of mother and father genes
-	new_generation.append(dad_Array)
-	new_generation.append(mum_Array)
-
-	for i in range(n-2): 
-		alpha = random()
-		beta  = 1 - alpha
-
-		mum_genes = np.dot(mum_Array, beta)
-		dad_genes = np.dot(dad_Array, alpha)
-		kid = dad_genes.__add__(mum_genes)
-		new_generation.append(kid)
-		print('alpha = {} beta = {}'.format(alpha, beta))
-		
-		
-	return new_generation 
 
 if __name__ == "__main__":
 	performance = train(100)
@@ -207,7 +174,6 @@ if __name__ == "__main__":
 	plt.legend(loc = 'upper right')
 	plt.xlabel('generation')
 	plt.show()
-
 	print('\n ----------------- \n average score: \n first generation = {} \n last generation = {}'.format(average[-1], average[1]))
 
 
